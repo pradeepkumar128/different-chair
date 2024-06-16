bf4a34db5e60cb614ca0e197c7644a702254f9b0
pradeepkumar128 <iampradeep1282@gmail.com>
2024-06-16 18:22:43 +0530

    project make done

---
+++ app.js
@@ -0,0 +1,20 @@
+function blueColor() {
+  document.body.style.backgroundColor = "#247aa1";
+  document.getElementById("imgbox").src = "images/blue.png";
+  document.getElementById("chairName").innerText = "Blue Chair";
+  document.body.style.color = "#fff";
+}
+
+function redColor() {
+  document.body.style.backgroundColor = "#80003c";
+  document.getElementById("imgbox").src = "images/maroon.png";
+  document.getElementById("chairName").innerText = "Maroon Chair";
+  document.body.style.color = "#fff";
+}
+
+function blackColor() {
+  document.body.style.backgroundColor = "#000";
+  document.getElementById("imgbox").src = "images/black.png";
+  document.getElementById("chairName").innerText = "Black Chair";
+  document.body.style.color = "#fff";
+}
