cc12718a5588ea81845c4a6eae7cd5ddee6c4664
pradeepkumar128 <iampradeep1282@gmail.com>
2024-06-16 18:27:30 +0530

    commleted

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 function blueColor() {
-  document.body.style.backgroundColor = "#247aa1";
-  document.getElementById("imgbox").src = "images/blue.png";
-  document.getElementById("chairName").innerText = "Blue Chair";
-  document.body.style.color = "#fff";
+setTimeout(()=>{
+     document.body.style.backgroundColor = "#247aa1";
+     document.getElementById("imgbox").src = "images/blue.png";
+     document.getElementById("chairName").innerText = "Blue Chair";
+     document.body.style.color = "#fff";
+},1000)
 }
 
 function redColor() {
+     setTimeout(()=>{
   document.body.style.backgroundColor = "#80003c";
   document.getElementById("imgbox").src = "images/maroon.png";
   document.getElementById("chairName").innerText = "Maroon Chair";
   document.body.style.color = "#fff";
+},1000)
 }
 
 function blackColor() {
+     setTimeout(()=>{
   document.body.style.backgroundColor = "#000";
   document.getElementById("imgbox").src = "images/black.png";
   document.getElementById("chairName").innerText = "Black Chair";
   document.body.style.color = "#fff";
+},1000)
 }
